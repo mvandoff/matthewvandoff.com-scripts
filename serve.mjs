bda7e8c4d4822ebb0873a089d9e053357e8d1c41
Matthew Van Doff <mvandoff@gmail.com>
2025-07-17 08:34:05 -0400

    Set up better esbuild server

---
+++ serve.mjs
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { glob } from 'glob';
+import esbuild from 'esbuild';
+import esbuildPluginInlineImport from 'esbuild-plugin-inline-import';
+import { transform } from 'lightningcss';
+
+const entryPoints = glob.sync('./src/**/*.ts');
+
+const ctx = await esbuild.context({
+	entryPoints,
+	outdir: 'dist',
+	bundle: true,
+	plugins: [
+		// Always include this plugin before others
+		esbuildPluginInlineImport({
+			transform: async (contents) => {
+				let { code } = transform({
+					code: Buffer.from(contents),
+					minify: true,
+				});
+				return code;
+			},
+		}),
+	],
+});
+
+await ctx.watch();
+
+const res = await ctx.serve({
+	servedir: 'dist',
+	keyfile: `./${process.env.SSL_KEY_FILE_NAME}`,
+	certfile: `./${process.env.SSL_CERT_FILE_NAME}`,
+});
+
+console.log(`Running on https://${res.hosts[1]}:${res.port}`);
